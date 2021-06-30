3bb85ab26d4d26fd913418aae33accd623ad89ef
Vojta Holik <vojta@egghead.io>
2021-06-30 14:28:00 +0200

    parse file names, 3 col grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,12 +15,21 @@ const lightboxOptions = {
   },
 }
 
-function getCaption(name: string) {
-  return name
-    .replace('@2x', '')
-    .replace('.jpg', '')
-    .replace('.png', '')
-    .replace('.jpeg', '')
+function getMetadata(name: string) {
+  const arr = name.split('-')
+  const caption = arr.length > 1 ? arr[1] : undefined
+  const index = arr[0]
+  function removeSuffix(string: string) {
+    return string
+      .replace('@2x', '')
+      .replace('.jpg', '')
+      .replace('.png', '')
+      .replace('.jpeg', '')
+  }
+  return {
+    caption: caption ? removeSuffix(caption) : undefined,
+    index: removeSuffix(index),
+  }
 }
 
 const LandingPage: React.FC<{ works: any }> = ({ works }) => {
@@ -40,13 +49,14 @@ const LandingPage: React.FC<{ works: any }> = ({ works }) => {
         </div>
       </div>
       <SRLWrapper options={lightboxOptions}>
-        <div className="grid w-full max-w-screen-xl lg:grid-cols-2 grid-cols-1 lg:gap-8 mx-auto">
+        <div className="grid w-full max-w-screen-2xl xl:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-8 mx-auto">
           {works.map((work: any, i: number) => {
-            const caption = getCaption(work.src)
+            const { caption, index } = getMetadata(work.src)
+            const key = caption || index
             return (
               <div
-                key={caption}
-                className={`py-16 flex flex-col items-center justify-center cursor-pointer`}
+                key={key}
+                className={`flex flex-col items-center justify-center cursor-pointer`}
               >
                 <div
                   className="flex items-center justify-center w-full group rounded-md overflow-hidden"
@@ -56,10 +66,13 @@ const LandingPage: React.FC<{ works: any }> = ({ works }) => {
                     placeholder="blur"
                     quality={50}
                     src={require(`../public/${IMGS_DIR}/${work.src}`)}
-                    alt={caption}
+                    alt={key}
                     className="group-hover:scale-105 transition-transform ease-in-out duration-300 rounded-md"
                     loading={loadAll ? 'eager' : 'lazy'}
                   />
+                </div>
+                <div className="pt-4 text-xs font-mono text-gray-600">
+                  {caption}
                 </div>
               </div>
             )
